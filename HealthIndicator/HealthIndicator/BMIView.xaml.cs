10aba18d776b95b39e868265acfe150fa645670c
avishrant <avishrant.stha@gmail.com>
2016-10-07 18:46:32 +1100

    MVVM
    demo for portfoio

---
+++ HealthIndicator/HealthIndicator/BMIView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using HealthIndicator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HealthIndicator.Views
+{
+    public partial class BMIView : ContentPage
+    {
+        public BMIView()
+        {
+            InitializeComponent();
+            this.BindingContext = new BMIViewModel();
+        }
+
+        
+    }
+    }
+
